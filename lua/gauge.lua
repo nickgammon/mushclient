bcb73dcdb0e555d863d28f7d017426d6449c3862
Nick Gammon <nick@gammon.com.au>
2010-02-17 15:26:46 +1100

    Improvements including handling of percent values

--- lua/gauge.lua
+++ lua/gauge.lua
@@ -10,46 +10,61 @@
   
   --]]
 
-function gauge (win,     -- miniwindow to draw in
-                name,    -- string, eg: "HP"
+function gauge (win,                        -- miniwindow ID to draw in
+                name,                       -- string, eg: "HP"
                 current, max,               -- current and max value (eg. 50, 100)
+                                            -- if max is nil, then current is a percentage
                 left, top, width, height,   -- where to put it inside the window
                 fg_colour, bg_colour,       -- colour for bar, colour for unfilled part
                 ticks, tick_colour,         -- number of ticks to draw, and in what colour
                 frame_colour)               -- colour for frame around bar
 
-  if max <= 0 then 
+  local Fraction
+  
+  if not current then
     return
-  end -- no divide by zero
-                
-  -- set up some defaults
-  height = height or 15
-  fg_colour = fg_colour or ColourNameToRGB "mediumblue"
-  bg_colour = bg_colour or ColourNameToRGB "rosybrown"
-  ticks = ticks or 5
-  tick_colour = tick_colour or ColourNameToRGB "silver"
-  frame_colour = frame_colour or ColourNameToRGB "lightgrey"
+  end -- if
+  
+  -- max == nil, means current is a percentage
+  if max then
+    if max <= 0 then 
+      return 
+    end -- no divide by zero
+
+    Fraction = current / max
+  else
+    Fraction = current / 100
+  end -- if 
   
   -- fraction in range 0 to 1
-  local Fraction = math.min (math.max (current / max, 0), 1) 
+  Fraction = math.min (math.max (Fraction, 0), 1) 
+  
+  -- set up some defaults
+  height        = height        or 15
+  fg_colour     = fg_colour     or ColourNameToRGB "mediumblue"
+  bg_colour     = bg_colour     or ColourNameToRGB "rosybrown"
+  ticks         = ticks         or 5
+  tick_colour   = tick_colour   or ColourNameToRGB "silver"
+  frame_colour  = frame_colour  or ColourNameToRGB "lightgrey"
 
   -- background colour - for un-filled part
   WindowRectOp (win, 2, left, top, left + width, top + height, bg_colour)  -- fill entire box
   
   -- how big filled part is
   local gauge_width = width * Fraction
-  
+    
    -- box size must be > 0 or WindowGradient fills the whole thing 
   if math.floor (gauge_width) > 0 then
     
     -- top half
-    WindowGradient (win, left, top, left + width, top + height / 2, 
+    WindowGradient (win, left, top, 
+                    left + gauge_width, top + height / 2, 
                     0x000000, -- black
                     fg_colour, 2)  -- vertical top to bottom
     
     -- bottom half
     WindowGradient (win, left, top + height / 2, 
-                    left + width, top +  height,   
+                    left + gauge_width, top + height,   
                     fg_colour,
                     0x000000, -- black
                     2) -- vertical top to bottom
@@ -81,9 +96,15 @@ function gauge (win,     -- miniwindow to draw in
                     "", "", "", "", "", "", 0, 0)
     end -- if
     
-    -- store numeric values in case they mouse over it
-    WindowHotspotTooltip(win, name, string.format ("%s\t%i / %i (%i%%)", 
-                          name, current, max, Fraction * 100) )
+      -- store numeric values in case they mouse over it
+    if max then
+      WindowHotspotTooltip(win, name, string.format ("%s\t%i / %i (%i%%)", 
+                            name, current, max, Fraction * 100) )
+    else
+      WindowHotspotTooltip(win, name, string.format ("%s\t(%i%%)", 
+                            name, Fraction * 100) )  
+    end -- if
+
   end -- hotspot wanted
                                     
 end -- function gauge
@@ -96,3 +117,26 @@ function max_text_width (win, font_id, t)
   end -- for each item
   return max
 end -- max_text_width
+
+-- get font from preferred font list
+function get_preferred_font (t)
+  local fonts = utils.getfontfamilies ()
+  
+  -- convert to upper-case
+  local f2 = {}
+  for k in pairs (fonts) do
+    f2 [k:upper ()] = true
+  end -- for
+  
+  for _, s in ipairs (t) do
+    if f2 [s:upper ()] then
+      return s
+    end -- if
+  end -- for each item
+
+  return "Courier"
+end -- get_preferred_font
+
+function capitalize (x)
+  return string.upper (string.sub(x, 1, 1)) .. string.lower (string.sub(x, 2))
+end -- capitalize
