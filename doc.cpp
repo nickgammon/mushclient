218794675d6757d14afc5dcb37e5ef6984a0281f
Nick Gammon <nick@gammon.com.au>
2016-02-29 16:47:30 +1100

    Fixed bug in Trigger management

--- doc.cpp
+++ doc.cpp
@@ -6112,7 +6112,7 @@ CTrigger * pTrigger;
 POSITION pos;
 
   GetTriggerArray ().SetSize (iCount);
-  CTriggerRevMap ().empty ();
+  CTriggerRevMap ().clear ();
 
   // extract pointers into a simple array
   for (i = 0, pos = GetTriggerMap ().GetStartPosition(); pos; i++)
