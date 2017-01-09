0fdad2355b8c3ab8498bede3cb3b39bac6ae2573
Nick Gammon <nick@gammon.com.au>
2017-01-10 08:30:44 +1100

    Added comments to explain things in version.h file

--- version.h
+++ version.h
@@ -1,6 +1,13 @@
+// Version number for this release. Used in various places.
+
+// Used for detecting version changes
 #define THISVERSION 505
+// Used in the resource file
 #define VERSION_16WORDS 5,0,5,0
+// Used to display the version number
 #define VERSION_STRING "5.05"
 
-// remember README
+// Note that the VERSION_STRING will have "-pre" appended by a pre-release build.
 
+// remember to edit date and version in readme.txt file
+//                  ^^^^^^^^^^^^^^^^
